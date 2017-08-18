9192ba78f3fcc865de694868c412606952edf7e2
lzhtls <www.136.om@qq.com>
2017-08-18 16:09:46 +0800

    SFTP
    This is a sftp socket ,for win !your can chang a little to linux ！

---
+++ my_sftp.py
@@ -0,0 +1,38 @@
+import paramiko
+import os
+
+'''pyhton for sftp !
+'''
+def sftp_upload(host,port,username,password,local,remote):
+    '''完成文件上传功能'''
+    sf = paramiko.Transport((host,port))
+    sf.connect(username = username,password = password)
+    sftp = paramiko.SFTPClient.from_transport(sf)
+    
+    os.chdir(local)             #切换到本地目录
+    files = os.listdir(local)   #轮询目录中的文件
+    for f in files:
+        sftp.put(os.path.join(local,f),os.path.join(remote,f))  #上传文件                              #上传文件
+    sf.close()
+
+        
+def sftp_download(host,port,username,password,local,remote):
+    '''完成文件下载功能'''
+    sf = paramiko.Transport((host,port))
+    sf.connect(username = username,password = password)
+    sftp = paramiko.SFTPClient.from_transport(sf)
+    
+    files = sftp.listdir(remote)
+    for f in files:
+        sftp.get(os.path.join(local,f),os.path.join(remote,f))  #下载文件
+    sf.close()
+
+if __name__ == '__main__':
+    host = '192.168.1.150'  #主机
+    port = 22               #端口
+    username = 'lzh'        #用户名
+    password = 'ts123456'   #密码
+    local = 'E:\\upahccb'                                   #本地目录
+    remote = '/'                                            #远程目录
+    sftp_upload(host,port,username,password,local,remote)   #上传
+    #sftp_download(host,port,username,password,local,remote)#下载
